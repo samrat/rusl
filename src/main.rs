9d1088156971f86920be4546fc7b4339a8f07cbb
Samrat Man Singh <samratmansingh@gmail.com>
2017-03-03 12:29:54 +0545

    WIP: parser
    - Build a tree: a nested list.
    - Now, the tree will get processed into the actual AST(with special forms)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use std::io;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Token {
     LParen,
     RParen,
-    Plus,
-    Minus,
-    
     Symbol(String),
     Number(i32),
     EOF,
@@ -15,71 +12,127 @@ enum Token {
 pub struct LexerState {
     s: String,
     pos: usize,
+    tok_buf: Option<Token>,
+}
+
+#[derive(Debug)]
+enum SExpr {
+    Symbol(String),
+    Number(i32),
+    List(Vec<SExpr>),
+}
+
+fn unread(ls: &mut LexerState, tok: Token) {
+    if let Some(_) = ls.tok_buf {
+        println!("error: unread buffer full");
+    }
+    else {
+        ls.tok_buf = Some(tok)
+    }
+}
+
+fn is_valid_symbol_start(c: char) -> bool {
+    // TODO: avoid allocatiing this in each call
+    let SymbolStartChars = vec!['+', '-', '*', '/'];
+
+    let mut ret = false;
+    if c.is_alphabetic() { ret = true; }
+    else {
+        for s in SymbolStartChars {
+            if c == s { ret = true; break; }
+            else { continue; }
+        }
+    }
+
+    return ret;
 }
 
 fn get_token(ls: &mut LexerState) -> Token {
-    let mut iter = ls.s[ls.pos..].chars().peekable();
-    while let Some(&c) = iter.peek() {
-        if c.is_numeric() {
-            let mut acc = String::new();
-            let mut n = c;
-            while n.is_numeric() {
-                acc.push(n);
-                iter.next();
-                ls.pos += 1;
-                n = match iter.peek() {
-                    Some(&x) => x,
-                    None => break,
-                };
-            }
-            return Token::Number(acc.parse().unwrap());
-        }
-        else if c.is_alphabetic() {
-            let mut acc = String::new();
-            let mut s = c;
-            while s.is_alphanumeric() {
-                acc.push(s);
-                iter.next();
-                ls.pos += 1;
-                s = match iter.peek() {
-                    Some(&x) => x,
-                    None => break,
-                };
-            }
-            return Token::Symbol(acc);
-        }
-        else {
-            match c {
-                ' ' => {
-                    iter.next();
-                    ls.pos += 1;
-                    continue
-                },
-                '(' => {
-                    iter.next();
-                    ls.pos += 1;
-                    return Token::LParen
-                },
-                ')' => {
-                    iter.next();
-                    ls.pos += 1;
-                    return Token::RParen
-                },
-                '+' => {
-                    iter.next();
-                    ls.pos += 1;
-                    return Token::Plus
-                },
-                '-' => {
-                    iter.next();
-                    ls.pos += 1;
-                    return Token::Minus
-                },
-                _ => return Token::EOF,
-            }
-        }
+    if let Some(tok) = ls.tok_buf.clone() {
+        ls.tok_buf = None;
+        return tok;
     }
-    return Token::EOF;
+    else {
+        let mut iter = ls.s[ls.pos..].chars().peekable();
+        while let Some(&c) = iter.peek() {
+            if c.is_numeric() {
+                let mut acc = String::new();
+                let mut n = c;
+                while n.is_numeric() {
+                    acc.push(n);
+                    iter.next();
+                    ls.pos += 1;
+                    n = match iter.peek() {
+                        Some(&x) => x,
+                        None => break,
+                    };
+                }
+                return Token::Number(acc.parse().unwrap());
+            }
+            else if is_valid_symbol_start(c) {
+                let mut acc = String::new();
+                let mut s = c;
+                while (s.is_alphanumeric() || 
+                       is_valid_symbol_start(s)) {
+                    acc.push(s);
+                    iter.next();
+                    ls.pos += 1;
+                    s = match iter.peek() {
+                        Some(&x) => x,
+                        None => break,
+                    };
+                }
+                return Token::Symbol(acc);
+            }
+            else {
+                match c {
+                    ' ' => {
+                        iter.next();
+                        ls.pos += 1;
+                        continue
+                    },
+                    '(' => {
+                        iter.next();
+                        ls.pos += 1;
+                        return Token::LParen
+                    },
+                    ')' => {
+                        iter.next();
+                        ls.pos += 1;
+                        return Token::RParen
+                    },
+                    _ => panic!("unexpected char: {}", c),
+                }
+            }
+        }
+        return Token::EOF;
+    }
+}
+
+fn get_list(ls: &mut LexerState) -> Vec<SExpr> {
+    match get_expr(ls) {
+        exp => match get_token(ls) {
+            Token::RParen => return vec![exp],
+            tok => {
+                unread(ls, tok);
+                let mut seq = get_list(ls);
+                seq.insert(0, exp);
+                return seq;
+            },
+        },
+    }
+}
+
+fn get_expr(ls: &mut LexerState) -> SExpr {
+    match get_token(ls) {
+        Token::Symbol(s) => return SExpr::Symbol(s),
+        Token::Number(n) => return SExpr::Number(n),
+        Token::LParen => {
+            return SExpr::List(get_list(ls));
+        },
+        Token::RParen => panic!("unmatched ')'"),
+        _ => return SExpr::Number(32),
+    }
 }
 
 fn read_input() -> io::Result<()> {
@@ -89,13 +142,11 @@ fn read_input() -> io::Result<()> {
 
     let mut lexer = LexerState {
         s: input,
-        pos: 0};
+        pos: 0,
+        tok_buf: None,
+    };
 
-    let mut tok = get_token(&mut lexer);
-    while tok != Token::EOF {
-        println!("get_token: {:?}", tok);
-        tok = get_token(&mut lexer);
-    }
+    println!("{:?}", get_expr(&mut lexer));
 
     Ok(())
 }
