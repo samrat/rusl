51c4376ba6aefe23f48d139f00ff78e881758208
Samrat Man Singh <samratmansingh@gmail.com>
2017-03-05 10:57:58 +0545

    Move parser to new file

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,15 @@ use std::io;
 use std::collections::HashMap;
 
 mod lexer;
+mod parser;
 
 use lexer::Token;
 use lexer::LexerState;
-use lexer::get_token;
 
+use parser::SExpr;
+use parser::get_expr;
+use parser::get_ast;
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-enum SExpr {
-    Symbol(String),
-    Number(i32),
-    Bool(bool),
-    List(Vec<SExpr>),
-
-    // TODO: change define to be special-form for function definition
-    Define(String, Box<SExpr>),
-    Let(Vec<(String, SExpr)>, Box<SExpr>),
-    If(Box<SExpr>, Box<SExpr>, Box<SExpr>),
-    App(Box<SExpr>, Vec<SExpr>),
-    Prog(Box<SExpr>),
-}
 
 #[derive(Clone, Debug)]
 enum Flat {
@@ -58,125 +47,7 @@ enum X86 {
     Prog(Vec<X86>, Vec<String>),
 }
 
-fn unread(ls: &mut LexerState, tok: Token) {
-    if let Some(_) = ls.tok_buf {
-        println!("error: unread buffer full");
-    }
-    else {
-        ls.tok_buf = Some(tok)
-    }
-}
 
-
-
-fn get_list(ls: &mut LexerState) -> Vec<SExpr> {
-    match get_expr(ls) {
-        exp => match get_token(ls) {
-            Token::RParen => return vec![exp],
-            tok => {
-                unread(ls, tok);
-                let mut seq = get_list(ls);
-                seq.insert(0, exp);
-                return seq;
-            },
-        },
-    }
-}
-
-fn get_expr(ls: &mut LexerState) -> SExpr {
-    match get_token(ls) {
-        Token::Symbol(s) => return SExpr::Symbol(s),
-        Token::Number(n) => return SExpr::Number(n),
-        Token::LParen => {
-            return SExpr::List(get_list(ls));
-        },
-        Token::RParen => panic!("unmatched ')'"),
-        _ => return SExpr::Number(32),
-    }
-}
-
-fn get_ast(expr: SExpr) -> SExpr {
-    match expr {
-        SExpr::Symbol(sym) => {
-            match &sym[..] {
-                "#f" => SExpr::Bool(false),
-                "#t" => SExpr::Bool(true),
-                _ => SExpr::Symbol(sym),
-            }
-        },
-        SExpr::List(elts) =>
-            match elts[0].clone() {
-                SExpr::Symbol(sym) => {
-                    match &sym[..] {
-                        "define" => {
-                            // TODO: Check that elts has correct length
-                            if let SExpr::Symbol(name) = elts[1].clone() {
-                                let e = elts[2].clone();
-                                return SExpr::Define(name, Box::new(e));
-                            }
-                            else {
-                                panic!("expected `name` to be a symbol");
-                            }
-                        },
-                        "if" => {
-                            return SExpr::If(Box::new(get_ast(elts[1].clone())),
-                                             Box::new(get_ast(elts[2].clone())),
-                                             Box::new(get_ast(elts[3].clone())));
-                        },
-                        "let" => {
-                            let bindings = match elts[1] {
-                                SExpr::List(ref bs) => bs,
-                                _ => panic!("let bindings"),
-                            };
-
-                            let mut astified_bindings = vec![];
-                            for bind_pair in bindings {
-                                let (key, val) = match bind_pair {
-                                    // TODO: check length
-                                    &SExpr::List(ref kv) => (kv[0].clone(), kv[1].clone()),
-                                    _ => panic!("non-list in let-binding"),
-                                };
-
-                                let keyname = match key {
-                                    SExpr::Symbol(k) => k,
-                                    _ => panic!("let binding key is not symbol"),
-                                };
-                                astified_bindings.push((keyname, get_ast(val)));
-                            }
-                            return SExpr::Let(astified_bindings, Box::new(get_ast(elts[2].clone())));
-                        },
-                        _ => {
-                            let mut astified_args = vec![];
-                            for arg in elts[1..].to_vec() {
-                                astified_args.push(get_ast(arg));
-                            }
-
-                            return SExpr::App(Box::new(elts[0].clone()), astified_args);
-                        },
-                    }
-                }
-                _ => panic!("NYI"),
-            },
-        _ => expr,
-    }
-}
-
-#[test]
-fn test_parser() {
-    let mut input = String::from("(if #f (+ 42 (foo 12)) 17)");
-    let mut lexer = LexerState {
-        s: input,
-        pos: 0,
-        tok_buf: None,
-    };
-    assert_eq!(SExpr::If(Box::new(SExpr::Bool(false)),
-                         Box::new(SExpr::App(Box::new(SExpr::Symbol("+".to_string())),
-                                             vec![SExpr::Number(42),
-                                                  SExpr::List(vec![SExpr::Symbol("foo".to_string()),
-                                                                   SExpr::Number(12)])])),
-                         Box::new(SExpr::Number(17))),
-               get_ast(get_expr(&mut lexer)));
-}
 
 
 static mut VAR_COUNTER : i32 = 0;
