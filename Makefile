efcef6ccf8d6d5b8c36629ee6f55d0ce7c3baa42
Samrat Man Singh <samratmansingh@gmail.com>
2017-09-01 12:06:02 +0545

    Add a Python script to run tests.
    The test suite is organized as a list of programs and their expected
    outputs. The input program is listed in a .input file and the expected
    output is listed in the .output file of the same name.
    
    The script compiles each input program, and checks the actual output
    against the expected output.

--- Makefile
+++ Makefile
@@ -3,7 +3,10 @@ SRC=examples
 test: runtime.o
 	cargo run $(SRC) > test.s
 	nasm -f elf64 test.s
-	gcc -o a.out runtime.c test.o
+	gcc -o a.out runtime.o test.o
 
 runtime.o: runtime.c
 	gcc -c -g -std=c99 runtime.c
+
+test_suite:
+	python run_tests.py
---
+++ run_tests.py
@@ -0,0 +1,70 @@
+import glob
+import subprocess
+import re
+import shutil
+import os
+
+"""
+USAGE: python run_tests.py
+
+Runs all tests in the `tests` directory.
+
+The way to specify a test is to add a pair of files- a .input file with 
+ the rusl program and a .output file with the expected output.
+"""
+
+def get_test_names():
+    input_files = glob.glob("tests/*.input")
+    test_names = list(map(lambda i: re.match(r"tests/(\w+)\.input", i).group(1), input_files))
+
+    return test_names
+
+def run_test(test_name):
+    output_path = "tests/" + test_name + ".output"
+    input_path  = "tests/" + test_name + ".input"
+    try:
+        output_file = open(output_path, "r")
+    except FileNotFoundError:
+        print("No corresponding .output file found for: " + test_name + ".input")
+        return 1
+
+    # TODO: Handle this for non-integer tests
+    expected_output = int(output_file.read())
+
+    # Compile and run the program
+    asm_path = "tests/_bin/" + test_name + ".s"
+    asm_file = open(asm_path, "w")
+    object_path = "tests/_bin/" + test_name + ".o"
+    bin_path = "tests/_bin/" + test_name + ".out"
+    subprocess.run(["cargo", "run", input_path], stdout=asm_file)
+    subprocess.run(["nasm", "-f", "elf64", asm_path])
+    subprocess.run(["gcc", "-o", bin_path, "runtime.c", object_path])
+
+    bin_run = subprocess.run(bin_path, stdout=subprocess.PIPE)
+
+    actual_output = int(bin_run.stdout)
+    if expected_output != actual_output:
+        print("Test %s failed. \t Expected: %d \t Actual: %d" %
+              (test_name, expected_output, actual_output))
+        return 1
+
+    return 0
+
+
+def run_all_tests(delete_generated_files=True):
+    test_names = get_test_names()
+    num_tests = len(test_names)
+    failures = 0
+    for test in test_names:
+        failures += run_test(test)
+
+    print("\n\n Ran %d tests. %d tests failed." % (num_tests, failures))
+    
+    # Delete generated files
+    if delete_generated_files:
+        shutil.rmtree("tests/_bin")
+
+
+if __name__ == '__main__':
+    os.mkdir("tests/_bin")
+    run_all_tests()        
